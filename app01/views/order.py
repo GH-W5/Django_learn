a73b52e06a0f0f4a7193e07a5e580fec9541b37a
cloudandsun <1961879865@qq.com>
2022-06-18 16:09:49 +0800

    订单功能-订单编辑，在页面上默认显示编辑内容

--- app01/views/order.py
+++ app01/views/order.py
@@ -82,8 +82,56 @@ def order_add(request):
 def order_delete(request):
     """ 删除订单 """
     uid = request.GET.get("uid")
-    exists =  models.Order.objects.filter(id=uid).exists()
+    exists = models.Order.objects.filter(id=uid).exists()
     if not exists:
         return JsonResponse({"status": False, "error": "数据不存在，删除失败。"})
     models.Order.objects.filter(id=uid).delete()
     return JsonResponse({"status": True})
+
+
+def order_ditail(request):
+    """ 根据ID获取订单详细 """
+    # 方式一
+    """ 
+    uid = request.GET.get("uid")
+    row_object = models.Order.objects.filter(id=uid).first()
+    if not row_object:
+        return JsonResponse({"status": False, "error": "数据不存在，编辑失败。"})
+
+    # 从数据库中获取到一个对象 row_object
+    result = {
+        "status": True,
+        "data": {
+            "title": row_object.title,
+            "price": row_object.price,
+            "status": row_object.status,
+        },
+    }
+    return JsonResponse(result)
+    """
+
+    # 方式二
+    uid = request.GET.get("uid")
+    row_dict = models.Order.objects.filter(id=uid).values("title", "price", "status").first()
+    if not row_dict:
+        return JsonResponse({"status": False, "error": "数据不存在，编辑失败。"})
+
+    result = {
+        "status": True,
+        "data": row_dict,
+    }
+    return JsonResponse(result)
+
+
+@csrf_exempt
+def order_edit(request):
+    """ 边界订单 （Ajax请求） """
+    uid = request.GET.get("uid")
+    row_object = models.Order.objects.filter(id=uid).first()
+    if not row_object:
+        return JsonResponse({"status": False, "tips": "数据不存在。"})
+    form = OrderModelForm(data=request.POST, instance=row_object)
+    if form.is_valid():
+        form.save()
+        return JsonResponse({"status": True})
+    return JsonResponse({"status": False, "error": form.errors})
